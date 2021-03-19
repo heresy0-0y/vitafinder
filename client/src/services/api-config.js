34d5d6ea14b7fe95caf106cd8b329d528118096f
hera sey <heresy.y.ysereh@gmail.com>
2021-03-19 14:48:00 -0500

    heroku to api config

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000";
+const baseUrl = "https://vitafinder-api.herokuapp.com/";
 // process.env.NODE_ENV === "production" ?
 // : "http://localhost:3000";
 
