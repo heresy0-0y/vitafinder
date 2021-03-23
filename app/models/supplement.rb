f5ca2377867d3887874c36fccf405300c9a4c155
hera sey <heresy.y.ysereh@gmail.com>
2021-03-23 15:20:27 -0500

    backend destroy fix

--- app/models/supplement.rb
+++ app/models/supplement.rb
@@ -1,5 +1,5 @@
 class Supplement < ApplicationRecord
   belongs_to :brand
   has_and_belongs_to_many :vitamins
-  has_many :vitamin_amount_per_supplements
+  has_many :vitamin_amount_per_supplements, dependent: :destroy
 end
